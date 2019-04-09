7baa047e5081b80914c3b9d29a4862deda833df2
Tomas Voskovec <tomas.voskovec@gmail.com>
2019-04-09 13:30:30 +0200

    init

---
+++ MusicBand/ClassLibrary1/MusicBand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    class MusicBand
+    {
+        public string Name { get; set; }
+        public int Year { get; set; }
+    }
+}
